d2dda31c5a9ac25c08933ecffa0cc646bbce8515
Mohamed Idrissi Alami <mohamedidrissialami22@gmail.com>
2025-09-14 23:32:33 +0100

    create game ui form

--- TicTacToeGame/Form1.Designer.cs
+++ TicTacToeGame/Form1.Designer.cs
@@ -28,13 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label66 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(348, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(607, 87);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic Tac Toe Game";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Gold;
+            this.button1.Location = new System.Drawing.Point(32, 537);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Reset Game";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label66
+            // 
+            this.label66.AutoSize = true;
+            this.label66.Font = new System.Drawing.Font("MV Boli", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label66.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label66.Location = new System.Drawing.Point(32, 167);
+            this.label66.Name = "label66";
+            this.label66.Size = new System.Drawing.Size(148, 70);
+            this.label66.TabIndex = 2;
+            this.label66.Text = "Turn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(32, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 52);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Player1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(32, 338);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 70);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Winner";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label5.Location = new System.Drawing.Point(32, 423);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 52);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "In Progress";
+            // 
+            // btn1
+            // 
+            this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn1.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Image = ((System.Drawing.Image)(resources.GetObject("btn1.Image")));
+            this.btn1.Location = new System.Drawing.Point(331, 158);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(200, 160);
+            this.btn1.TabIndex = 6;
+            this.btn1.Tag = "?";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Image = ((System.Drawing.Image)(resources.GetObject("btn2.Image")));
+            this.btn2.Location = new System.Drawing.Point(568, 158);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(200, 160);
+            this.btn2.TabIndex = 7;
+            this.btn2.Tag = "?";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn3.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Image = ((System.Drawing.Image)(resources.GetObject("btn3.Image")));
+            this.btn3.Location = new System.Drawing.Point(805, 158);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(200, 160);
+            this.btn3.TabIndex = 8;
+            this.btn3.Tag = "?";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn4.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Image = ((System.Drawing.Image)(resources.GetObject("btn4.Image")));
+            this.btn4.Location = new System.Drawing.Point(331, 356);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(200, 160);
+            this.btn4.TabIndex = 9;
+            this.btn4.Tag = "?";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn5.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Image = ((System.Drawing.Image)(resources.GetObject("btn5.Image")));
+            this.btn5.Location = new System.Drawing.Point(568, 356);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(200, 160);
+            this.btn5.TabIndex = 10;
+            this.btn5.Tag = "?";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn6.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Image = ((System.Drawing.Image)(resources.GetObject("btn6.Image")));
+            this.btn6.Location = new System.Drawing.Point(805, 356);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(200, 160);
+            this.btn6.TabIndex = 11;
+            this.btn6.Tag = "?";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn7.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Image = ((System.Drawing.Image)(resources.GetObject("btn7.Image")));
+            this.btn7.Location = new System.Drawing.Point(331, 554);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(200, 160);
+            this.btn7.TabIndex = 12;
+            this.btn7.Tag = "?";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn8.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Image = ((System.Drawing.Image)(resources.GetObject("btn8.Image")));
+            this.btn8.Location = new System.Drawing.Point(568, 554);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(200, 160);
+            this.btn8.TabIndex = 13;
+            this.btn8.Tag = "?";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn9.FlatAppearance.BorderColor = System.Drawing.Color.Azure;
+            this.btn9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gold;
+            this.btn9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Image = ((System.Drawing.Image)(resources.GetObject("btn9.Image")));
+            this.btn9.Location = new System.Drawing.Point(805, 554);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(200, 160);
+            this.btn9.TabIndex = 14;
+            this.btn9.Tag = "?";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1102, 747);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label66);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label66;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
     }
 }
 
